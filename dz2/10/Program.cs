a12453a5f2427d49b39ff135e472d18945e687fa
Inna <inna.kibalina@gmail.com>
2022-11-02 13:55:21 +0700

    решение 10й задачи

---
+++ dz2/10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int GenerateRandomNum()
+{
+    return new Random ().Next(100, 1000);
+}
+
+int SaveSecondDigit (int randomnumber)
+{
+     int firstNum = randomnumber / 10;
+     return firstNum % 10;
+}
+int randomnumber = GenerateRandomNum();
+     Console.WriteLine(randomnumber);
+     Console.WriteLine(SaveSecondDigit(randomnumber));
